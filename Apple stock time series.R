d032b625666360b23d4aedbca90410d37e517246
kartik4887 <160771004+kartik4887@users.noreply.github.com>
2024-03-04 23:22:54 +0530

    Update Apple stock time series.R

--- Apple stock time series.R
+++ Apple stock time series.R
@@ -38,80 +38,79 @@ plot(tsr)
 
 # *******************************************************************
 
-adf_test_aapl = adf.test(apple_price); adf_test_aapl 
+adf_test_aapl = adf.test(apple_price); adf_test_aapl
 
-aapl_ds = diff(apple_price); plot(aapl_ds) 
+aapl_ret = na.omit(diff(log(apple_price)))
+adf_test_aapl_ret = adf.test(aapl_ret); adf_test_aapl_ret
 
-adf_test_aapl_ds = adf.test(aapl_ds); adf_test_aapl_ds
-
-lb_test_aapl_ds = Box.test(aapl_ds); lb_test_aapl_ds 
+plot(aapl_ret)
+# Ljung-Box Test for Autocorrelation - Stock Data (H0: No Autocorrelation)
+# ***********************************************
+lb_test_aapl_ret = Box.test(aapl_ret); lb_test_aapl_ret 
 
 acf(apple_price) # ACF of Series
 pacf(apple_price) # PACF of Series
 
-acf(aapl_ds) # ACF of  Difference (Stationary) Series
-pacf(aapl_ds) # PACF of  Difference (Stationary) Series
+acf(aapl_ret) # ACF of returns
+pacf(aapl_ret) # PACF of returns
 
 # Auto ARIMA
-arma_pq_aapl_ds = auto.arima(aapl_ds); arma_pq_aapl_ds
+arma_pq_aapl_ret = auto.arima(aapl_ret); arma_pq_aapl_ret
 arma_pq_aapl = auto.arima(apple_price); arma_pq_aapl
 
 # Ljung-Box Test for Autocorrelation - Model Residuals(H0: No Autocorrelation)
 # ***************************************************
-lb_test_arma_pq_aapl_ds = Box.test(arma_pq_aapl_ds$residuals); lb_test_arma_pq_aapl_ds
+lb_test_arma_pq_aapl_ret = Box.test(arma_pq_aapl_ret$residuals); lb_test_arma_pq_aapl_ret
 
 
 # ARIMA (1, 0, 0) or AR(1)
-ar1 = arima(aapl_ds, order = c(1, 0, 0)); ar1
+ar1 = arima(aapl_ret, order = c(1, 0, 0)); ar1
 
 # ARIMA (2, 0, 0) or AR(2)
-ar2 = arima(aapl_ds, order = c(2, 0, 0)); ar2
+ar2 = arima(aapl_ret, order = c(2, 0, 0)); ar2
 
 # ARIMA (0, 0 , 1) or MA(1)
-ma1 = arima(aapl_ds, order = c(0, 0, 1)); ma1
+ma1 = arima(aapl_ret, order = c(0, 0, 1)); ma1
 
 # ARIMA (0, 0, 2) or MA(2)
-ma2 = arima(aapl_ds, order = c(0, 0, 2)); ma2
+ma2 = arima(aapl_ret, order = c(0, 0, 2)); ma2
 
 # ARIMA (0, 0, 3) or MA(3)
-ma3 = arima(aapl_ds, order = c(0, 0, 3)); ma3
+ma3 = arima(aapl_ret, order = c(0, 0, 3)); ma3
 
 # ARIMA (0, 0, 4) or MA(4)
-ma4 = arima(aapl_ds, order = c(0, 0, 4)); ma4
+ma4 = arima(aapl_ret, order = c(0, 0, 4)); ma4
 
 # ARIMA (1, 0, 1) or ARMA(1, 1)
-arma11 = arima(aapl_ds, order = c(1, 0, 1)); arma11
+arma11 = arima(aapl_ret, order = c(1, 0, 1)); arma11
 
 # ARIMA (1, 0, 2) or ARMA(1, 2)
-arma12 = arima(aapl_ds, order = c(1, 0, 2)); arma12
+arma12 = arima(aapl_ret, order = c(1, 0, 2)); arma12
 
 # ARIMA (1, 0, 3) or ARMA(1, 3)
-arma13 = arima(aapl_ds, order = c(1, 0, 3)); arma13
+arma13 = arima(aapl_ret, order = c(1, 0, 3)); arma13
 
 # 3.1.2. Forecasting with ARIMA Models
 # ************************************
-aapl_ds_f11 = predict(arma11, n.ahead = 40)
-plot(aapl_ds_f11)
-lines(aapl_ds_f11$pred, col = 'blue')
-lines(aapl_ds_f11$pred + 2 * aapl_ds_f11$se, col = 'red')
-lines(aapl_ds_f11$pred - 2 * aapl_ds_f11$se, col = 'red')
 
-aapl_ds_fpq = forecast(arma_pq_aapl_ds, h = 40)
-plot(aapl_ds_fpq)
+aapl_ret_fpq = forecast(arma_pq_aapl_ret, h = 40)
+plot(aapl_ret_fpq)
 
 aapl_fpq = forecast(arma_pq_aapl, h = 40)
 plot(aapl_fpq)
 
-aapl_ret = na.omit(diff(log(apple_price))) 
+ 
 plot(aapl_ret)
+# Test for Volatility Clustering or Heteroskedasticity: Box Test
 
-aapl_ret_sq = aapl_ret^2 # Return Variance (Since Mean Returns is approx. 0)
+aapl_ret_sq = arma_pq_aapl_ret$residuals^2 # Return Variance (Since Mean Returns is approx. 0)
 plot(aapl_ret_sq)
+
 aapl_ret_sq_box_test = Box.test(aapl_ret_sq, lag = 10) # H0: Return Variance Series is Not Serially Correlated
 aapl_ret_sq_box_test # Inference : Return Variance Series is Heteroskedastic (Has Volatility Clustering)
 
 # Test for Volatility Clustering or Heteroskedasticity: ARCH Test
-aapl_ret_arch_test = ArchTest(aapl_ret, lags = 20) # H0: No ARCH Effects
+aapl_ret_arch_test = ArchTest(arma_pq_aapl_ret$residuals^2, lags = 10) # H0: No ARCH Effects
 aapl_ret_arch_test # Inference : Return Series is Heteroskedastic (Has Volatility Clustering)
 
 
@@ -125,3 +124,5 @@ aapl_ret_garch2 = ugarchfit(garch_model2, data = aapl_ret); aapl_ret_garch2
 # GARCH Forecast
 aapl_ret_garch_forecast1 = ugarchforecast(aapl_ret_garch1, n.ahead = 50); aapl_ret_garch_forecast1
 aapl_ret_garch_forecast2 = ugarchforecast(aapl_ret_garch2, n.ahead = 50); aapl_ret_garch_forecast2
+
+plot(aapl_ret_garch_forecast1)
